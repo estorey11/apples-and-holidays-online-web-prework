14746987fd426bbfd014ed90339119246e74ba19
Eric Storey <estorey@gmail.com>
2019-03-11 03:08:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,6 +74,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each {|season, holiday |
     puts "#{season.capitalize}:"
     holiday.each {|holiday, supplies|
+      word_day="#{holiday}"
+      word_day.
       prints "  "
     }
   }
