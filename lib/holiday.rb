c36953a46a7a7f16dfc529dedb936ce8cb6a9928
Eric Storey <estorey@gmail.com>
2019-03-11 03:39:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -95,7 +95,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  bbq_names=[]
+  holiday_supplies.each {|season, holiday|
+    holiday.each {|holiday, supplies|
+      
+    }
+  }
 end
 
 holiday_supplies = {
